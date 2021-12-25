aa1ba8fd26c824d6f71a638e87309143204d5365
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-12-25 18:15:09 +0800

    That's all.

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+import fs from "fs/promises";
+import fsextra from "fs-extra";
+import path from "path";
+const uploadDir = "upload";
+fsextra.ensureDir(uploadDir);
+const app = express();
+const port = 3070;
+
+app.use(express.static("public"));
+app.use(fileUpload({}));
+
+app.post("/upload", async (req, res) => {
+  const { files } = req;
+  if (files) {
+    const flattened = Array.from(Object.values(files)).flat();
+    await Promise.all(
+      flattened.map((file) =>
+        fs.writeFile(path.join(uploadDir, file.name), file.data)
+      )
+    );
+    res.end('<script>alert("Uploaded!");window.location.href = "/"</script>');
+  } else {
+    res.end(
+      '<script>alert("No files uploaded!");window.location.href = "/"</script>'
+    );
+  }
+});
+
+app.listen(port, () => {
+  console.log("uploader started.");
+});
