3be7eb7706b537ffd37fc0075663af37593dc506
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-09-20 19:59:08 +0800

    feat: add im uploader

--- index.ts
+++ index.ts
@@ -3,38 +3,48 @@ import fileUpload from "express-fileupload";
 import fsextra from "fs-extra";
 import path from "path";
 import child_process from "child_process";
+import expressWS from "express-ws";
 import ip from "ip";
 import open from "open";
+import dotenv from "dotenv";
 const uploadDir = "upload";
+dotenv.config();
 fsextra.ensureDir(uploadDir);
-const app = express();
-const port = 3070;
-function getMeta() {
+const app = expressWS(express()).app;
+const port = +(process.env.PORT ?? "3080");
+function getMeta(req: express.Request) {
   const ipv4 = ip.address("public", "ipv4");
+  const host = ipv4;
   const ipv6 = ip.address("public", "ipv6");
   const meta = {
     ipv4,
     ipv6,
     port,
-    quickLink: `http://${ipv4}:${port}`,
+    lastPaste,
+    room,
+    ip: req.ip,
+    quickLink: `http://${host}:${port}`,
   };
   return meta;
 }
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(fileUpload({}));
+const endMessage = (res: express.Response, msg: string) => {
+  res.end(`<script>alert('${msg}');window.location.href = "/"</script>`);
+};
 app.post("/upload", async (req, res) => {
   const { files } = req;
   if (files) {
     const flattened = Array.from(Object.values(files)).flat();
     await Promise.all(flattened.map((file) => fsextra.writeFile(path.join(uploadDir, file.name), file.data)));
-    res.end('<script>alert("Uploaded!");window.location.href = "/"</script>');
+    endMessage(res, "Uploaded!");
   } else {
-    res.end('<script>alert("No files uploaded!");window.location.href = "/"</script>');
+    endMessage(res, "No files uploaded!");
   }
 });
-app.get("/", (_, res) => {
-  const meta = getMeta();
+app.get("/", (req, res) => {
+  const meta = getMeta(req);
   res.render("index", meta);
 });
 app.post("/~invoke/explorer", (_, res) => {
@@ -43,7 +53,70 @@ app.post("/~invoke/explorer", (_, res) => {
   });
   res.status(204).end();
 });
+
+let lastPaste = "";
+app.post("/paste", (req, res) => {
+  const content: string = req.body.content;
+  console.debug(content);
+  lastPaste = content;
+  res.redirect("/");
+});
+interface Member {
+  ip: string;
+  send: (msg: Uint8Array) => void;
+}
+
+const room: Member[] = [];
+app.ws("/im", (ws, req) => {
+  const ip = req.ip;
+  const send = (msg: Uint8Array) => {
+    console.debug(`sending to ${ip}`);
+    ws.send(msg);
+  };
+  room.push({
+    ip,
+    send,
+  });
+  console.debug(`added member ${ip}`);
+  ws.on("close", () => {
+    room.splice(room.findIndex((item) => item.ip === ip));
+    console.debug(`removed member ${ip}`);
+  });
+});
+app.post("/chat-file", (req, res) => {
+  const { files } = req;
+  const ip = req.body.ip;
+  const targetMember = room.find((m) => m.ip === ip);
+  if (!targetMember) {
+    endMessage(res, `member ${ip} is not present`);
+    return;
+  }
+  if (files) {
+    const allFiles = Array.from(Object.values(files)).flat();
+    for (const file of allFiles) {
+      const { data, encoding, mimetype, name } = file;
+      const meta = {
+        encoding,
+        mimetype,
+        fileName: name,
+      };
+      const jsonBin = Buffer.from(JSON.stringify(meta), "utf-8");
+      const metaSize = 1 << 10;
+      if (jsonBin.length > metaSize) {
+        console.warn("meta data size exceeded", meta);
+        continue;
+      }
+      const buf = Buffer.alloc(data.length + metaSize);
+      buf.set(jsonBin, 0);
+      buf.set(data, metaSize);
+      targetMember.send(buf);
+    }
+    endMessage(res, "Chat file posted.");
+  } else {
+    endMessage(res, "No file selected.");
+  }
+});
 app.listen(port, () => {
-  console.log("uploader started.");
+  console.info("Uploader started.");
   open(`http://localhost:${port}`);
 });
--- public/js/setup.js
+++ public/js/setup.js
@@ -8,13 +8,33 @@ window.quickLink = document.querySelector("meta[name='quick-link']").content;
  */
 async function copyToClipboard(text) {
   const copyContent = text || window.ipv4;
-  await window.navigator.clipboard.writeText(copyContent);
-  alert(`Copied ${copyContent} to clipboard!`);
+  try {
+    await window.navigator.clipboard.writeText(copyContent);
+    alert(`Copied ${copyContent} to clipboard!`);
+  } catch (error) {
+    alert(error.message);
+  }
 }
 async function openFolder() {
   await fetch("/~invoke/explorer", {
     method: "post",
   });
+}
+function showSize() {
+  alert(
+    JSON.stringify(
+      {
+        "window.innerHeight": window.innerHeight,
+        "window.innerWidth": window.innerWidth,
+        "window.outerHeight": window.outerHeight,
+        "window.outerWidth": window.outerWidth,
+        "document.documentElement.offsetHeight": document.documentElement.offsetHeight,
+        "document.documentElement.offsetWidth": document.documentElement.offsetWidth,
+      },
+      undefined,
+      2
+    )
+  );
 }
 window.addEventListener("load", () => {
   const qrcode = new QRCode(document.getElementById("qrcode"), {
@@ -23,3 +43,43 @@ window.addEventListener("load", () => {
   });
   qrcode.makeCode(quickLink);
 });
+const ws = new WebSocket(`ws://${location.host}/im`);
+const wsReady = new Promise((resolve) => {
+  ws.addEventListener("open", () => {
+    console.debug("WebSocket connected.");
+    resolve();
+  });
+});
+ws.addEventListener("message", (e) => {
+  const data = e.data;
+  if (data instanceof Blob) {
+    const metaSize = 1 << 10;
+    const reader = new FileReader();
+    const metaPart = data.slice(0, metaSize);
+    reader.readAsText(metaPart, "utf-8");
+    reader.onloadend = () => {
+      /** @type {string} */
+      const result = reader.result;
+      const meta = JSON.parse([...result].filter((char) => char.charCodeAt(0)).join(""));
+      console.debug(`Parsed metadata:`, meta);
+      const dataPart = data.slice(metaSize);
+      const url = URL.createObjectURL(new Blob([dataPart], { type: meta.mimetype }));
+      const link = document.createElement("a");
+      link.href = url;
+      link.appendChild(new Text(meta.fileName));
+      document.body.appendChild(link);
+    };
+  }
+});
+async function send(ip) {
+  const formWrapper = document.getElementById("chat-form-wrapper");
+  formWrapper.querySelector("input").value = ip;
+  const form = formWrapper.querySelector("form");
+  const formdata = new FormData(form);
+  // use fetch API for easier multi post.
+  await fetch("/chat-file", {
+    method: "post",
+    body: formdata,
+  });
+  alert(`Files are posted to ${ip}.`);
+}
